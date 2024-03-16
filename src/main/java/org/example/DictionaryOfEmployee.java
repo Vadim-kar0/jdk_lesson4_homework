8768ccde1c2b0935865ac8ddcec0a30e3a13aae3
вадим <funpalka1@gmail.com>
2024-03-16 21:47:22 +0400

    Переделал через стримы

--- src/main/java/org/example/DictionaryOfEmployee.java
+++ src/main/java/org/example/DictionaryOfEmployee.java
@@ -3,6 +3,7 @@ package org.example;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DictionaryOfEmployee {
     private List<Employee> employees;
@@ -24,30 +25,34 @@ public class DictionaryOfEmployee {
     }
 
     public List<Employee> findEmployeeByExperience(int experience){
-        List<Employee> result = new ArrayList<>();
-        for (Employee employee : employees) {
-            if(employee.experience() == experience){
-                result.add(employee);
-            }
-        }
-        return result;
+        //List<Employee> result = new ArrayList<>();
+        //for (Employee employee : employees) {
+        //    if(employee.experience() == experience){
+        //        result.add(employee);
+        //    }
+        //}
+        //return result;
+        return employees.stream().filter(s -> s.experience() == experience).collect(Collectors.toList());
     }
     public List<String> findNumberByName(String name){
-        List<String> result = new ArrayList<>();
-        for (Employee employee : employees) {
-            if(employee.name().equals(name)){
-                result.add(employee.phoneNumber());
-            }
-        }
-        return result;
+        //List<String> result = new ArrayList<>();
+        //for (Employee employee : employees) {
+        //    if(employee.name().equals(name)){
+        //        result.add(employee.phoneNumber());
+        //    }
+        //}
+        //return result;
+        return employees.stream().filter(s -> s.name().equals(name))
+                .map(Employee::phoneNumber).collect(Collectors.toList());
     }
     public Employee findEmployeeByID(int id){
-        for (Employee employee : employees) {
-            if(employee.id() == id){
-                return employee;
-            }
-        }
-        return null;
+        //for (Employee employee : employees) {
+        //    if(employee.id() == id){
+        //        return employee;
+        //    }
+        //}
+        //return null;
+        return employees.stream().filter(s -> s.id() == id).findAny().orElse(null);
     }
     public void addNewEmployee(Employee employee){
         employees.add(employee);
